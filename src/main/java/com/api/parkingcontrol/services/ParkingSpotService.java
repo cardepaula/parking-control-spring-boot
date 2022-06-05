e87313d5e4a071641661c55ba7fd7d5ee85f347e
Marcos A C de Paula <cardepaula@gmail.com>
2022-06-05 17:43:19 -0300

    criaçao da api rest com spring boot

---
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,54 @@
+package com.api.parkingcontrol.services;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+
+@Service
+public class ParkingSpotService {
+
+	final ParkingSpotRepository parkingSpotRepository;
+
+	public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+		this.parkingSpotRepository = parkingSpotRepository;
+	}
+
+	@Transactional
+	public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+		return parkingSpotRepository.save(parkingSpotModel);
+	}
+
+	public boolean existsByLicensePlateCar(String licensePlateCar) {
+		return this.parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
+	}
+
+	public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+		return this.parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
+	}
+
+	public boolean existsByApartmentAndBlock(String apartment, String block) {
+		return this.parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
+	}
+
+	public Page<ParkingSpotModel> findAll(Pageable pageable) {
+		return this.parkingSpotRepository.findAll(pageable);
+	}
+
+	public Optional<ParkingSpotModel> findById(UUID id) {
+		return this.parkingSpotRepository.findById(id);
+	}
+
+	@Transactional
+	public void delete(ParkingSpotModel parkingSpotModel) {
+		this.parkingSpotRepository.delete(parkingSpotModel);
+	}
+
+}
